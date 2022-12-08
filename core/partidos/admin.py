bedef68a3db7a6e420ceec433c3ec1e97203c84a
marvinbraga <mvbraga@gmail.com>
2022-12-08 11:13:20 -0300

    Admin do partido.

--- core/partidos/admin.py
+++ core/partidos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.partidos.models import Partido
+
+
+class PartidoAdmin(admin.ModelAdmin):
+    list_display = ["name", "short_name", "number", "type"]
+    list_display_links = ["short_name", "number"]
+
+
+admin.site.register(Partido, PartidoAdmin)
--- core/partidos/models.py
+++ core/partidos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from core.commons.models import IdTimeStampedModel
 from django.utils.translation import gettext_lazy as _
+
+from core.commons.models import IdTimeStampedModel
 
 
 class EspectroPolitico(models.IntegerChoices):
@@ -12,14 +13,25 @@ class EspectroPolitico(models.IntegerChoices):
 
 
 class Partido(IdTimeStampedModel):
-    name = models.CharField(verbose_name=_("Nome"), max_length=250, unique=True)
+    name = models.CharField(
+        verbose_name=_("Nome"),
+        max_length=250,
+        unique=True,
+        help_text="",
+    )
     short_name = models.CharField(verbose_name=_("Sigla"), max_length=15, unique=True)
     number = models.IntegerField(verbose_name=_("Número"), unique=True)
     type = models.IntegerField(
-        verbose_name=_("Espectro"), choices=EspectroPolitico.choices,
-        default=EspectroPolitico.NENHUM
+        verbose_name=_("Espectro"),
+        choices=EspectroPolitico.choices,
+        default=EspectroPolitico.NENHUM,
     )
-    description = models.CharField(verbose_name=_("Descrição"), max_length=250, blank=True, null=True)
+    description = models.CharField(
+        verbose_name=_("Descrição"),
+        max_length=250,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return f"{self.short_name} ({self.number})"
